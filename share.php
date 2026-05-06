698255a055f1f28c85deb562892feeeaf4bf7277
stilesloveland-cyber <stilesloveland@gmail.com>
2026-05-06 19:59:16 +0800

    213

--- share.php
+++ share.php
@@ -124,6 +124,12 @@ if (isset($_GET['id'])) {
         'js' => 'application/javascript; charset=utf-8',
         'json' => 'application/json; charset=utf-8',
         'pdf' => 'application/pdf',
+        'doc' => 'application/msword',
+        'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'xls' => 'application/vnd.ms-excel',
+        'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'ppt' => 'application/vnd.ms-powerpoint',
+        'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
         'mp4' => 'video/mp4', 'webm' => 'video/webm',
         'mp3' => 'audio/mpeg', 'wav' => 'audio/wav',
         'flac' => 'audio/flac', 'ogg' => 'audio/ogg'
